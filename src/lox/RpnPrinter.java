c0ee26b12a35077edc83a13942c98df2b51fb5eb
Sarakuzoi <calin.andrei.radoi@gmail.com>
2024-03-28 15:52:46 +0100

    Added RPN Visitor

--- src/lox/RpnPrinter.java
+++ src/lox/RpnPrinter.java
@@ -1,27 +1,33 @@
 package lox;
 
-public class AstPrinter implements Expr.Visitor<String> {
+import java.util.Stack;
+
+public class RpnPrinter implements  Expr.Visitor<String> {
 
     public static void main(String[] args) {
         Expr expression = new Expr.Binary(
-                new Expr.Unary(
-                        new Token(TokenType.MINUS, "-", null, 1),
-                        new Expr.Literal(123)),
+                new Expr.Binary(
+                        new Expr.Literal(1),
+                        new Token(TokenType.PLUS, "+", null, 1),
+                        new Expr.Literal(2)
+                ),
                 new Token(TokenType.STAR, "*", null, 1),
-                new Expr.Grouping(
-                        new Expr.Literal(45.67)));
+                new Expr.Binary(
+                        new Expr.Literal(4),
+                        new Token(TokenType.MINUS, "-", null, 1),
+                        new Expr.Literal(3)
+                )
+        );
 
-        System.out.println(new AstPrinter().print(expression));
+        System.out.println(new RpnPrinter().print(expression));
     }
 
     String print(Expr expr) {
         return expr.accept(this);
     }
-
     @Override
     public String visitBinaryExpr(Expr.Binary expr) {
-        return parenthesize(expr.operator.lexeme,
-                expr.left, expr.right);
+        return parenthesize(expr.operator.lexeme, expr.left, expr.right);
     }
 
     @Override
@@ -43,14 +49,13 @@ public class AstPrinter implements Expr.Visitor<String> {
     private String parenthesize(String name, Expr... exprs) {
         StringBuilder builder = new StringBuilder();
 
-        builder.append("(").append(name);
         for (Expr expr : exprs) {
-            builder.append(" ");
             builder.append(expr.accept(this));
+            builder.append(" ");
         }
-        builder.append(")");
+        builder.append(name);
+        builder.append(" ");
 
         return builder.toString();
     }
-
 }
