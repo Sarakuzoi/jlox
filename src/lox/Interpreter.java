3791da7e2f1a6f4a955aeb6614c7e573bea291db
Sarakuzoi <calin.andrei.radoi@gmail.com>
2024-07-27 15:18:48 +0300

    Wired the resolver to the interpreter

--- src/lox/Interpreter.java
+++ src/lox/Interpreter.java
@@ -1,8 +1,6 @@
 package lox;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
@@ -10,6 +8,7 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     private static class ContinueException extends RuntimeException {}
     final Environment globals = new Environment();
     private Environment environment = globals;
+    private final Map<Expr, Integer> locals = new HashMap<>();
     private boolean repl = false;
 
     Interpreter() {
@@ -59,6 +58,11 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         }
         stmt.accept(this);
     }
+
+    void resolve(Expr expr, int depth) {
+        locals.put(expr, depth);
+    }
+
 
     void executeBlock(List<Stmt> statements,
                       Environment environment) {
@@ -309,16 +313,30 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     @Override
     public Object visitAssignExpr(Expr.Assign expr) {
         Object value = evaluate(expr.value);
-        environment.assign(expr.name, value);
+
+        Integer distance = locals.get(expr);
+        if (distance != null) {
+            environment.assignAt(distance, expr.name, value);
+        } else {
+            globals.assign(expr.name, value);
+        }
+
         return value;
     }
 
     @Override
     public Object visitVariableExpr(Expr.Variable expr) {
-        return environment.get(expr.name);
+        return lookUpVariable(expr.name, expr);
     }
 
-
+    private Object lookUpVariable(Token name, Expr expr) {
+        Integer distance = locals.get(expr);
+        if (distance != null) {
+            return environment.getAt(distance, name.lexeme);
+        } else {
+            return globals.get(name);
+        }
+    }
 
     private Object evaluate(Expr expr) {
         return expr.accept(this);
