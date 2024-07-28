a8440f5c11a4cc41a6b49f24df9c498cb9a03253
Sarakuzoi <calin.andrei.radoi@gmail.com>
2024-07-28 23:34:13 +0300

    Added visitor method for interpreter, resolver. Implemented constructors

---
+++ src/lox/LoxClass.java
@@ -0,0 +1,28 @@
+package lox;
+
+import java.util.List;
+
+class LoxClass implements LoxCallable {
+    final String name;
+
+    LoxClass(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public Object call(Interpreter interpreter,
+                       List<Object> arguments) {
+        LoxInstance instance = new LoxInstance(this);
+        return instance;
+    }
+
+    @Override
+    public int arity() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
